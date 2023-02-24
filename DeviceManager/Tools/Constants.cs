ef0c3033e60bf3e5ff77706eccc640e21f4c36d5
Gabriel Lara Baptista <gabriel.l.baptista@gmail.com>
2023-02-23 23:09:44 -0300

    Versão final da Demonstração

---
+++ DeviceManager/Tools/Constants.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DeviceManager.Tools
+{
+    internal static class Constants
+    {
+        public const string CS_DEVICE_01 = "HostName=iot-vs-summit.azure-devices.net;DeviceId=device01;SharedAccessKey=[Device01Key]";
+        public const string CS_DEVICE_02 = "HostName=iot-vs-summit.azure-devices.net;DeviceId=device02;SharedAccessKey=[Device02Key]";
+        public const string CS_DEVICE_03 = "HostName=iot-vs-summit.azure-devices.net;DeviceId=device03;SharedAccessKey=[Device03Key]";
+        public const string CS_DEVICE_04 = "HostName=iot-vs-summit.azure-devices.net;DeviceId=device04;SharedAccessKey=[Device04Key]";
+        public const string CS_DEVICE_05 = "HostName=iot-vs-summit.azure-devices.net;DeviceId=device05;SharedAccessKey=[Device05Key]";
+
+    }
+}
